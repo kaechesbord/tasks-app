3dd0b6b5a30760b3e9a930cbbb2615b47542c545
kaechesbord <dmemic29@gmail.com>
2023-06-06 21:06:28 +0200

    more added

--- src/App.js
+++ src/App.js
@@ -12,30 +12,31 @@ const totalDuration = calculateTimeDuration();
 export const mockData = [
   {
     date: 'Today',
-    when: DateTime.local().startOf('day').valueOf(),
+    when: DateTime.local().startOf('day'),
     time: totalDuration,
   },
   {
     date: 'Tomorrow',
-    when: DateTime.local().plus({ days: 1 }).startOf('day').valueOf(),
+    when: DateTime.local().plus({ days: 1 }).startOf('day'),
     time: totalDuration,
   },
   {
     date: 'September 22',
-    when: DateTime.fromObject({ year: 2023, month: 9, day: 22 }).startOf('day').valueOf(),
+    when: DateTime.fromObject({ year: 2023, month: 9, day: 22 }).startOf('day'),
     time: totalDuration,
   },
 ];
 
 
-
 const show = true
 const shifts = mockDataTime.length;
 const App = () => {
+  console.log(mockData)
   return (
     <div className="app-container">
       <div className="content-container">
         {mockData.map((data, index) => (
+
           <div key={index}>
             <Header date={data.date} shifts={shifts} time={data.time} showElement={show} />
             <Tab date={data.date} started={false} />
--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,48 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import './Tab.css';
-import { DateTime } from 'luxon';
-import { v4 as uuidv4 } from 'uuid';
-import { mockData } from '../App';
+import React, { useEffect, useState } from "react";
+import "./Tab.css";
+import { DateTime } from "luxon";
+import { mockData } from "../App";
 
-const createMockShift = (values) =>
-  Object.assign(
-    {
-      id: uuidv4(),
-      booked: false,
-    },
-    values
-  );
-
-const currentDate = DateTime.local().startOf('day');
+const currentDate = DateTime.local().startOf("day");
 
 export const mockDataTime = [
-  createMockShift({
-    area: 'Helsinki',
-    startTime: currentDate.set({ hour: 19 }).valueOf(),
-    endTime: currentDate.set({ hour: 20 }).valueOf(),
-  }),
-  createMockShift({
-    area: 'Turbe',
-    startTime: currentDate.set({ hour: 10 }).valueOf(),
-    endTime: currentDate.set({ hour: 12 }).valueOf(),
-  }),
-  createMockShift({
-    area: 'Stockholm',
-    startTime: currentDate.set({ hour: 13 }).valueOf(),
-    endTime: currentDate.set({ hour: 14, minutes: 30 }).valueOf(),
-  }),
-  createMockShift({
-    area: 'Turku',
-    startTime: currentDate.set({ hour: 16 }).valueOf(),
-    endTime: currentDate.set({ hour: 17, minutes: 30 }).valueOf(),
-  }),
-  createMockShift({
-    area: 'Antananarivo',
-    startTime: currentDate.set({ hour: 20 }).valueOf(),
-    endTime: currentDate.set({ hour: 23, minutes: 30 }).valueOf(),
-  }),
+  {
+    id: "95a2aaca-bab8-4504-8646-f75b325ec0e7",
+    booked: false,
+    area: "Helsinki",
+    startTime: 1523610000000,
+    endTime: 1523617200000,
+  },
+  {
+    id: "001e40e5-05dc-4b9d-bdc5-cae63f651970",
+    booked: true,
+    area: "Tampere",
+    startTime: 1523602800000,
+    endTime: 1523610000000,
+  },
+  {
+    id: "c5a7c672-3036-47d4-9f08-5cfe94f23067",
+    booked: false,
+    area: "Helsinki",
+    startTime: 1523610000000,
+    endTime: 1523617200000,
+  },
+  {
+    id: "8f245d57-0b16-4e6f-af92-904a1a2e7127",
+    booked: true,
+    area: "Tampere",
+    startTime: 1523602800000,
+    endTime: 1523610000000,
+  },
+  {
+    id: "0b7658e7-2e83-4a32-93fe-d8a22a1e17ae",
+    booked: false,
+    area: "Helsinki",
+    startTime: 1523610000000,
+    endTime: 1523617200000,
+  },
+  {
+    id: "43738cf0-791b-4765-b7a4-491fd49ccf36",
+    booked: true,
+    area: "Tampere",
+    startTime: 1686054052000,
+    endTime: 1686078752000
+  },
+  {
+    id: "c9d791c0-2a1d-4f20-8e4b-4e5f56f7b0e0",
+    booked: false,
+    area: "Helsinki",
+    startTime: 1523602800000,
+    endTime: 1523610000000,
+  }
 ];
-
 export function calculateTimeDuration() {
   let totalDuration = 0;
 
@@ -50,44 +63,51 @@ export function calculateTimeDuration() {
     const duration = item.endTime - item.startTime;
     totalDuration += duration;
   });
-
-  return Math.round((totalDuration / 3600000) * 10) / 10;;
+  return Math.round((totalDuration / 3600000) * 10) / 10;
 }
 
 const Tab = ({ startTime, endTime, location }) => {
   const [isStarted, setStarted] = useState(false);
-  const [today, setToday] = useState()
 
-  const formattedStartTime = DateTime.fromMillis(startTime).toFormat('HH:mm');
-  const formattedEndTime = DateTime.fromMillis(endTime).toFormat('HH:mm');
+  const formattedStartTime = DateTime.fromMillis(startTime).toFormat("HH:mm");
+  const formattedEndTime = DateTime.fromMillis(endTime).toFormat("HH:mm");
+  let currentTime = Date.now()
+
   useEffect(() => {
-    const currentTime = DateTime.local().toMillis();
-    location = currentTime
-    const startTimestamp = startTime;
-    const endTimestamp = endTime;
-    mockData.forEach((data) => {
-      if(currentDate.hasSame(data.when, 'day')) {
-        setToday(true)
-      }
-    })
-    if(today) {
-      if (currentTime >= startTimestamp && currentTime <= endTimestamp) {
-        setStarted(true);
-      } else {
-        setStarted(false);
+    const startTimestamp = startTime.valueOf();
+    const endTimestamp = endTime.valueOf();
+    let foundToday = false;
+  
+    for (const data of mockData) {
+      if (currentDate.toMillis() === data.when.toMillis()) {
+        foundToday = true;
+        break; // Exit the current loop
       }
     }
-  }, [startTime, endTime, location]);
+  
+    if (!foundToday) {
+      return; // Exit the useEffect hook if no match is found
+    }
+  
+    if (currentTime >= startTimestamp && currentTime <= endTimestamp) {
+      setStarted(true);
+    }
+  }, [startTime, endTime, currentTime, currentDate, mockData]);
+  
+  
+  
 
   return (
     <div>
-      <div className='tab'>
-        <div className='text'>
-          <h2 className='time'>{formattedStartTime} - {formattedEndTime}</h2>
-          <h4 className='location'>{location}</h4>
+      <div className="tab">
+        <div className="text">
+          <h2 className="time">
+            {formattedStartTime} - {formattedEndTime}
+          </h2>
+          <h4 className="location">{location}</h4>
         </div>
-        <div className='btn'>
-          <button className={isStarted ? 'btn-started' : 'btn-notStarted'}>
+        <div className="btn">
+          <button className={isStarted ? "btn-started" : "btn-notStarted"}>
             Cancel
           </button>
         </div>
@@ -113,4 +133,3 @@ const TabContainer = () => {
 };
 
 export default TabContainer;
-
